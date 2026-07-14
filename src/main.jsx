639f4c981f983a6b0b7158d55c7cf37eb3333f48
Dorcas Muthoni <muthonidorcasa.c.c4540@gmail.com>
2026-07-14 15:45:01 +0300

    Added Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import "./index.css";
 import "./styles/global.css";
 
 import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
